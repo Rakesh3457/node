040f036b26b9352a21d9f1a1edecc8e9f5a9258a
Rakesh3457 <113657841+Rakesh3457@users.noreply.github.com>
2023-05-07 18:30:02 +0530

    Add files via upload
    body parser

--- new.js
+++ new.js
@@ -1,17 +1,26 @@
 //const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req, res, next)=>{
+app.use('/add-product',(req, res, next)=>{
    console.log('its first middleware');
-  res.send('<h1>lets dive into express.js</h1>');
-   next();
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">submit</button></form>');
+   
+   
 });
 
-app.use((req, res, next)=>{
-   console.log('its second middleware');
-  
-});
+app.use('/product',(req, res, next)=>{
+   console.log(req.body);
+   res.redirect('/');
+  });
+  app.use('/',(req, res, next)=>{
+   console.log('task done');
+   
+  });
 app.listen(5500);
